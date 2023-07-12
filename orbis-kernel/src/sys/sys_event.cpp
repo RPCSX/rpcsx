985ba2d6fbc11f166abcc27030b6f48f0220d288
DH <dh.rpcs3@gmail.com>
2023-07-12 04:20:28 +0300

    [orbis-kernel] Stub kevent

--- orbis-kernel/src/sys/sys_event.cpp
+++ orbis-kernel/src/sys/sys_event.cpp
@@ -1,10 +1,26 @@
 #include "sys/sysproto.hpp"
 
-orbis::SysResult orbis::sys_kqueue(Thread *thread) { return {}; }
+#include "thread/Process.hpp"
+#include "utils/Logs.hpp"
+
+struct KQueue : orbis::RcBase {};
+
+orbis::SysResult orbis::sys_kqueue(Thread *thread) {
+  ORBIS_LOG_TODO(__FUNCTION__);
+  auto queue = knew<KQueue>();
+  if (queue == nullptr) {
+    return ErrorCode::NOMEM;
+  }
+
+  thread->retval[0] = thread->tproc->fileDescriptors.insert(queue);
+  return {};
+}
 
 orbis::SysResult orbis::sys_kevent(Thread *thread, sint fd,
-                                   ptr<struct kevent> changelist, sint nchanges,
-                                   ptr<struct kevent> eventlist, sint nevents,
-                                   ptr<const struct timespec> timeout) {
+                                   ptr<KEvent> changelist, sint nchanges,
+                                   ptr<KEvent> eventlist, sint nevents,
+                                   ptr<const timespec> timeout) {
+  // ORBIS_LOG_TODO(__FUNCTION__, fd);
+  thread->retval[0] = 1;
   return {};
 }
