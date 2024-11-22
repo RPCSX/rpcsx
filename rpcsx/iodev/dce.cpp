8559a3d5dd34112c8dfb0875e7d8d68f34ccab5c
DH <dh.rpcs3@gmail.com>
2024-11-22 16:13:24 +0300

    ps5: dce: implement buffer in use reporting

--- rpcsx/iodev/dce.cpp
+++ rpcsx/iodev/dce.cpp
@@ -356,6 +356,14 @@ static orbis::ErrorCode dce_ioctl(orbis::File *file, std::uint64_t request,
         return {};
       }
 
+      if (args->id == 0x33) {
+        if (args->arg2 == 0 && args->ptr == nullptr && args->arg5 == 0 &&
+            args->arg6 == 0) {
+          gpuCtx.bufferInUseAddress[thread->tproc->vmId] = args->size;
+          return {};
+        }
+      }
+
       ORBIS_LOG_FATAL("dce: unimplemented 0x80308217 request", args->id,
                       args->padding, args->arg2, args->ptr, args->size,
                       args->arg5, args->arg6);
